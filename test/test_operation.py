8af7a75afa4c05b1bbfc1e85cf377c8cfbb163cd
GILFROSH <smartking3077@gmail.com>
2024-09-06 15:02:21 +0100

    unit test stuff

---
+++ test/test_operation.py
@@ -0,0 +1,17 @@
+from src.math_stuff import add, sub
+
+
+def test_add():
+    assert add(2,3) ==  5
+    assert add(-1,0) ==  -1
+    assert add(4,-3) ==  1
+
+
+def test_add():
+    assert sub(2,3) ==  -1
+    assert sub(-1,0) ==  -1
+    assert sub(4,-3) ==  7
+
+
+
+
